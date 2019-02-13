7f881b42ab6e05fab6893cc3dce6e20b7a1e8fd8
Manasa Vesala <vesalamanasa@gmail.com>
2019-02-12 16:59:43 -0800

    Added router, and CSS

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Ticket(props){
-
   return (
     <div>
+      <style jsx>{`
+          div {
+            background-color: aqua;
+          }
+        `}</style>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
